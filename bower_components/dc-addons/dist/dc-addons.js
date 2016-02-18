8abd327c8a0b632db566820f731339b3eb6a6aa0
Timothy Ruhle <timothy.ruhle@intellipharm.com.au>
2016-02-18 16:27:30 +1000

    Bumping to v0.12.0

--- bower_components/dc-addons/dist/dc-addons.js
+++ bower_components/dc-addons/dist/dc-addons.js
@@ -1,7 +1,7 @@
 /*!
- * dc-addons v0.11.5
+ * dc-addons v0.12.0
  *
- * 2016-02-08 09:21:40
+ * 2016-02-18 16:19:52
  *
  */
 if (!dc.utils.getAllFilters) {
@@ -609,6 +609,62 @@ if (!dc.utils.getAllFilters) {
                 _chart.filter(filter);
                 dc.redrawAll(_chart.chartGroup());
             });
+        };
+
+        return _chart.anchor(parent, chartGroup);
+    };
+})();
+
+(function () {
+    'use strict';
+
+    if (dc.leafletCustomChart) {
+        return false;
+    }
+
+    dc.leafletCustomChart = function (parent, chartGroup) {
+        var _chart = dc.baseLeafletChart({});
+
+        var _redrawItem = null;
+        var _renderItem = null;
+
+        _chart.renderTitle(true);
+
+        _chart._postRender = function () {
+            var data = _chart._computeOrderedGroups(_chart.data());
+
+            data.forEach(function (d, i) {
+                _chart.renderItem()(_chart, _chart.map(), d, i);
+            });
+        };
+
+        _chart._doRedraw = function () {
+            var data = _chart._computeOrderedGroups(_chart.data());
+
+            var accessor = _chart.valueAccessor();
+
+            data.forEach(function (d, i) {
+                d.filtered = accessor(d) === 0;
+                _chart.redrawItem()(_chart, _chart.map(), d, i);
+            });
+        };
+
+        _chart.redrawItem = function (_) {
+            if (!arguments.length) {
+                return _redrawItem;
+            }
+
+            _redrawItem = _;
+            return _chart;
+        };
+
+        _chart.renderItem = function (_) {
+            if (!arguments.length) {
+                return _renderItem;
+            }
+
+            _renderItem = _;
+            return _chart;
         };
 
         return _chart.anchor(parent, chartGroup);
@@ -1280,6 +1336,62 @@ dc.leafletLegend = function () {
                 _chart.filter(filter);
                 dc.redrawAll(_chart.chartGroup());
             });
+        };
+
+        return _chart.anchor(parent, chartGroup);
+    };
+})();
+
+(function () {
+    'use strict';
+
+    if (dc.googleCustomChart) {
+        return false;
+    }
+
+    dc.googleCustomChart = function (parent, chartGroup) {
+        var _chart = dc.baseGoogleChart({});
+
+        var _redrawItem = null;
+        var _renderItem = null;
+
+        _chart.renderTitle(true);
+
+        _chart._postRender = function () {
+            var data = _chart._computeOrderedGroups(_chart.data());
+
+            data.forEach(function (d, i) {
+                _chart.renderItem()(_chart, _chart.map(), d, i);
+            });
+        };
+
+        _chart._doRedraw = function () {
+            var data = _chart._computeOrderedGroups(_chart.data());
+
+            var accessor = _chart.valueAccessor();
+
+            data.forEach(function (d, i) {
+                d.filtered = accessor(d) === 0;
+                _chart.redrawItem()(_chart, _chart.map(), d, i);
+            });
+        };
+
+        _chart.redrawItem = function (_) {
+            if (!arguments.length) {
+                return _redrawItem;
+            }
+
+            _redrawItem = _;
+            return _chart;
+        };
+
+        _chart.renderItem = function (_) {
+            if (!arguments.length) {
+                return _renderItem;
+            }
+
+            _renderItem = _;
+            return _chart;
         };
 
         return _chart.anchor(parent, chartGroup);
